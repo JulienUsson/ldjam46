f0ff9bdfda72bdc847f215dac2b5d9ef320c267a
Julien Usson <julien.usson@easymov.fr>
2020-04-19 13:49:55 +0200

    better bios

--- src/utils/randomBio.ts
+++ src/utils/randomBio.ts
@@ -7,11 +7,13 @@ const deadQuotes: Array<(isMale: boolean) => string> = [
   (isMale) => `don't turn off his phone in a movie`,
   (isMale) => `not pick up dog poop when walking ${isMale ? 'his' : 'her'} dog`,
   (isMale) => `drive slow in the fast lane on the freeway`,
-  (isMale) => `clear ${isMale ? 'his' : 'her'} throat in public`,
-  (isMale) => `has voted Trump at last election`,
   (isMale) => `is not racist because ${isMale ? 'he' : 'she'} has a black friend`,
   (isMale) => `use always the wrong meme`,
   (isMale) => `seen all Twilight films and loved it`,
+  (isMale) => `thinks unicorns are real`,
+  (isMale) => `believe in cat shaped aliens`,
+  (isMale) => `knows 4 decimal digits of Pi`,
+  (isMale) => `is born with 3 niples`,
 ]
 
 export default function randomBio(sex: 'male' | 'female') {
